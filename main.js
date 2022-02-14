093056f34a3537ba4bb512f6aa3a8996dd757e08
mayankshahi <mayank.shahi@gmail.com>
2022-02-14 17:18:21 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,50 @@
+function more_universe()
+{
+    document.getElementById("more_universe").innerHTML = "The earliest cosmological models of the universe were developed by ancient Greek and Indian philosophers and were geocentric, placing Earth at the center. Over the centuries, more precise astronomical observations led Nicolaus Copernicus to develop the heliocentric model with the Sun at the center of the Solar System. In developing the law of universal gravitation, Isaac Newton built upon Copernicus's work as well as Johannes Kepler's laws of planetary motion and observations by Tycho Brahe."
+    document.getElementById("blablabla").innerHTML  =  "  "
+}
+
+function sun_open()
+{
+    window.scrollTo(0, document.getElementById('sun').offsetTop);
+}
+
+function mercury_open()
+{
+    window.scrollTo(0, document.getElementById('mercury').offsetTop);
+}
+
+function venus_open()
+{
+    window.scrollTo(0, document.getElementById('venus').offsetTop);
+}
+
+function earth_open()
+{
+    window.scrollTo(0, document.getElementById('earth').offsetTop);
+}
+
+function mars_open()
+{
+    window.scrollTo(0, document.getElementById('mars').offsetTop);
+}
+
+function jupiter_open()
+{
+    window.scrollTo(0, document.getElementById('jupiter').offsetTop);
+}
+
+function saturn_open()
+{
+    window.scrollTo(0, document.getElementById('saturn').offsetTop);
+}
+
+function uranus_open()
+{
+    window.scrollTo(0, document.getElementById('uranus').offsetTop);
+}
+
+function neptune_open()
+{
+    window.scrollTo(0, document.getElementById('neptune').offsetTop);
+}
